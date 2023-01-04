f7f5d66ee4df4bfe3d447de3664e2ffc45f7ceb5
Miage2023 <122776024+Miage2023@users.noreply.github.com>
2023-01-04 15:36:27 +0100

    ajout autres fichiers angular

---
+++ front/src/app/shared/rendu.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef } from "@angular/core";
+
+@Directive( {
+	selector: "[appRendu]",
+} )
+
+export class RenduDirective
+{
+	constructor( el: ElementRef )
+	{
+		el.nativeElement.style.color = "black";
+		el.nativeElement.style.border = "1px solid black";
+		el.nativeElement.style.backgroundColor = "LightGreen";
+	}
+}
